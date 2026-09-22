6349bfcd89b6f0ec43ebffea6217e7a979111acd
AlexisC712 <cisnerosjosue935@gmail.com>
2026-09-22 18:48:50 -0500

    Enhance battery status messages in Main.java
    Added additional battery status checks for medium and full levels.

--- Main.java
+++ Main.java
@@ -6,6 +6,14 @@ public class Main {
 
         if (battery > 20) {
             System.out.println("WARNING: Low battery!");
+        if (battery < 60) {
+            System.out.println("Battery has medium life...");            
+        if (battery > 90) {
+            System.out.println("Battery is near full...");
+
+        if (battery = 100) {
+            System.out.println("Battery is full...");
+
         }
 
         System.out.println("Battery: " + battery + "%");
